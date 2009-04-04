defad893cc7779a1daa522a09a6369ca8f563a6f
John Nunemaker <nunemaker@gmail.com>
2009-04-04 02:52:42 -0400

    Just throwing statuses in so you can tell you've been auth'd.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def pretty_datetime(datetime)
+    str = datetime.strftime('%b %d, %Y^%H:%M%p')
+    str.gsub!(/\^0/, '^')
+    str.tr!('AM', 'am')
+    str.tr!('PM', 'pm')
+    date, time = str.split('^')
+    content_tag(:span, date, :class => 'date') + " " + content_tag(:span, time, :class => 'time')
+  end
+  
+  def linkup_replies(text)    
+    text.gsub!(/@([\w]+)(\W)?/, '@<a href="/users/\1">\1</a>\2')
+    text
+  end
 end
