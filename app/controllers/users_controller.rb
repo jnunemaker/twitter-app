3cae40a8c86bd0431d2fad91b4479fa66b768b6b
John Nunemaker <nunemaker@gmail.com>
2009-04-04 22:00:11 -0400

    Added user profile page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
   include Clearance::App::Controllers::UsersController
+  
+  def show
+    @tweets = current_user.client.user_timeline(:id => params[:id])
+    
+    @user = @tweets.size > 0 ? 
+              @tweets.first.user : 
+              current_user.client.user(params[:id])
+  end
 end
