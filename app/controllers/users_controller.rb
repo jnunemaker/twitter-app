c79faf34193715d3fa92a733b8d955173e887660
John Nunemaker <nunemaker@gmail.com>
2009-04-14 17:14:00 -0400

    Updated to latest version of twitter gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @tweets = current_user.client.user_timeline(:id => params[:id])
-    
-    @user = @tweets.size > 0 ? 
-              @tweets.first.user : 
-              current_user.client.user(params[:id])
+    @user = current_user.client.user(params[:id])
   end
   
   private
