794551ae431f348fe3d503e624a5814b1451c725
StuFF mc <mc@stuffmc.com>
2010-09-02 19:21:57 +0800

    Upgraded to 2.3.8 and gem 0.9.8 + added a usefull (i.m.h.o) Token display for beginners to be sure they passed the ENV variables correctly. Added the syntax in README.rdoc

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,13 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'twitter', :version => '0.9.1'
+  config.gem 'twitter', :version => '0.9.8'
   config.frameworks -= [:active_record, :active_resource]
   config.time_zone = 'UTC'
 end
